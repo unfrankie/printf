263bc41a5c6df16060602353a298a2abd842fc74
Anass El Omari <anas.iramol@gmail.com>
2023-10-14 14:07:08 -0700

    Im not going anywhere. You can print that wherever you want to. Im here and Im a Spur for life

--- main.h
+++ main.h
@@ -5,13 +5,13 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_character(va_list l);
-int print_string(va_list l);
-int _strlen(const char *s);
+int _putchar(char);
+int _printf(const char *, ...);
+int print_character(va_list);
+int print_string(va_list);
+int _strlen(const char *);
 int print_percentage(void);
-int print_integer(va_list l);
-int print_decimal(va_list l)
+int print_integer(va_list);
+int print_decimal(va_list)
 
 #endif
