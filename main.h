9fa59df311ddf75248a7486cd6fdaca1fc4e9465
Anass El Omari <anas.iramol@gmail.com>
2023-10-15 07:45:47 -0700

    printf

--- main.h
+++ main.h
@@ -5,16 +5,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int _printf(const char *format, ...);
 
-int _putchar(char);
-int _printf(const char *, ...);
-int handler(const char *, va_list);
-int handler_type(const char *, va_list, int *);
-int print_character(va_list);
-int print_string(va_list);
-int _strlen(const char *);
-int print_percentage(void);
-int print_integer(va_list);
-int print_decimal(va_list)
 
 #endif
