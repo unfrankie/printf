f3577cbd24a3701ae2ec8f551e3342f01c41222d
Anass El Omari <anas.iramol@gmail.com>
2023-10-15 12:00:12 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -87,12 +87,10 @@ int print_number(int n)
 
 int _printf(const char *format, ...)
 {
-	int printf(const char *format, ...)
-{
 	int j = 0, va_list l;
-	
+
 	va_start(l, format);
-	if (!format || !format[0]);
+	if (!format || !format[0])
 		return (-1);
 	while (*format)
 	{
@@ -102,11 +100,13 @@ int _printf(const char *format, ...)
 			if (*format == 'c')
 			{
 				char c = va_arg(l, int);
+
 				j += _putchar(c);
 			}
 			else if (*format == 's')
 			{
 				char *s = va_arg(l, char *);
+
 				j += print_string(s);
 			}
 			else if (*format == '%')
@@ -117,6 +117,7 @@ int _printf(const char *format, ...)
 			else if (*format == 'd' || *format == 'i')
 			{
 				int n = va_arg(l, int);
+
 				if (n < 0)
 					j++;
 				j += length_number(n);
